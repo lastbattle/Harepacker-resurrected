5a2c7891fa2c70af25bb623074472f97193ea36e
eaxvac <github_lastbattle@outlook.com>
2018-06-10 04:17:07 +0800

    Management of Map.wz fieldLimitType via checkboxes

--- HaRepackerLib/Controls/HaRepackerMainPanels/HaRepackerMainPanel.Designer.cs
+++ HaRepackerLib/Controls/HaRepackerMainPanels/HaRepackerMainPanel.Designer.cs
@@ -42,10 +42,12 @@
             this.textPropBox = new System.Windows.Forms.TextBox();
             this.pictureBoxPanel = new System.Windows.Forms.Panel();
             this.canvasPropBox = new System.Windows.Forms.PictureBox();
+            this.listView_fieldLimitType = new System.Windows.Forms.ListView();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.selectionLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.mainProgressBar = new System.Windows.Forms.ToolStripProgressBar();
             this.secondaryProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel_additionalInfo = new System.Windows.Forms.ToolStripStatusLabel();
             this.findStrip = new System.Windows.Forms.ToolStrip();
             this.btnFindAll = new System.Windows.Forms.ToolStripButton();
             this.btnFindNext = new System.Windows.Forms.ToolStripButton();
@@ -54,7 +56,6 @@
             this.btnClose = new System.Windows.Forms.ToolStripButton();
             this.btnOptions = new System.Windows.Forms.ToolStripButton();
             this.MainDockPanel = new WeifenLuo.WinFormsUI.Docking.DockPanel();
-            this.toolStripStatusLabel_additionalInfo = new System.Windows.Forms.ToolStripStatusLabel();
             ((System.ComponentModel.ISupportInitialize)(this.MainSplitContainer)).BeginInit();
             this.MainSplitContainer.Panel1.SuspendLayout();
             this.MainSplitContainer.Panel2.SuspendLayout();
@@ -86,6 +87,7 @@
             this.MainSplitContainer.Panel2.Controls.Add(this.mp3Player);
             this.MainSplitContainer.Panel2.Controls.Add(this.textPropBox);
             this.MainSplitContainer.Panel2.Controls.Add(this.pictureBoxPanel);
+            this.MainSplitContainer.Panel2.Controls.Add(this.listView_fieldLimitType);
             this.MainSplitContainer.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.MainSplitContainer_SplitterMoved);
             // 
             // DataTree
@@ -169,6 +171,21 @@
             this.canvasPropBox.Name = "canvasPropBox";
             this.canvasPropBox.TabStop = false;
             // 
+            // listView_fieldLimitType
+            // 
+            resources.ApplyResources(this.listView_fieldLimitType, "listView_fieldLimitType");
+            this.listView_fieldLimitType.CheckBoxes = true;
+            this.listView_fieldLimitType.FullRowSelect = true;
+            this.listView_fieldLimitType.GridLines = true;
+            this.listView_fieldLimitType.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView_fieldLimitType.Name = "listView_fieldLimitType";
+            this.listView_fieldLimitType.ShowGroups = false;
+            this.listView_fieldLimitType.ShowItemToolTips = true;
+            this.listView_fieldLimitType.UseCompatibleStateImageBehavior = false;
+            this.listView_fieldLimitType.View = System.Windows.Forms.View.Details;
+            this.listView_fieldLimitType.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.ListView_fieldLimitType_ItemCheck);
+            this.listView_fieldLimitType.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.ListView_fieldLimitType_ItemChecked);
+            // 
             // statusStrip
             // 
             this.statusStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
@@ -194,6 +211,12 @@
             // 
             this.secondaryProgressBar.Name = "secondaryProgressBar";
             resources.ApplyResources(this.secondaryProgressBar, "secondaryProgressBar");
+            // 
+            // toolStripStatusLabel_additionalInfo
+            // 
+            this.toolStripStatusLabel_additionalInfo.Margin = new System.Windows.Forms.Padding(200, 3, 0, 2);
+            this.toolStripStatusLabel_additionalInfo.Name = "toolStripStatusLabel_additionalInfo";
+            resources.ApplyResources(this.toolStripStatusLabel_additionalInfo, "toolStripStatusLabel_additionalInfo");
             // 
             // findStrip
             // 
@@ -261,12 +284,6 @@
             resources.ApplyResources(this.MainDockPanel, "MainDockPanel");
             this.MainDockPanel.Name = "MainDockPanel";
             // 
-            // toolStripStatusLabel_additionalInfo
-            // 
-            this.toolStripStatusLabel_additionalInfo.Margin = new System.Windows.Forms.Padding(200, 3, 0, 2);
-            this.toolStripStatusLabel_additionalInfo.Name = "toolStripStatusLabel_additionalInfo";
-            resources.ApplyResources(this.toolStripStatusLabel_additionalInfo, "toolStripStatusLabel_additionalInfo");
-            // 
             // HaRepackerMainPanel
             // 
             resources.ApplyResources(this, "$this");
@@ -321,5 +338,6 @@
         public TreeViewMS.TreeViewMS DataTree;
         private WeifenLuo.WinFormsUI.Docking.DockPanel MainDockPanel;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_additionalInfo;
+        private System.Windows.Forms.ListView listView_fieldLimitType;
     }
 }
---
+++ MapleLib/WzLib/WzStructure/Data/WzFieldLimitType.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapleLib.WzLib.WzStructure.Data
+{
+    public enum WzFieldLimitType
+    {
+        //Unable_To_Use_AntiMacro_Item = 0, // CField::IsUnableToUseAntiMacroItem = CField *this 
+        Unable_To_Jump = 0, // CField::IsUnableToJump = CField *this 
+        Unable_To_Use_Skill = 1, // CField::IsUnableToUseSkill = CField *this 
+        Unable_To_Use_Summon_Item = 2, // CField::IsUnableToUseSummonItem = CField *this 
+        Unable_To_Use_Mystic_Door = 3, // CField::IsUnableToUseMysticDoor = CField *this 
+        Unable_To_Migrate = 4, // CField::IsUnableToMigrate = CField *this 
+        Unable_To_Use_Portal_Scroll = 5, // CField::IsUnableToUsePortalScroll = CField *this 
+        Unable_To_Use_Teleport_Item = 6, // CField::IsUnableToUseTeleportItem = CField *this 
+        Unable_To_Open_Mini_Game = 7, // CField::IsUnableToOpenMiniGame = CField *this 
+        Unable_To_Use_Specific_Portal_Scroll = 8, // CField::IsUnableToUseSpecificPortalScroll = CField *this 
+        Unable_To_Use_Taming_Mob = 9, //  CField::IsUnableToUseTamingMob = CField *this 
+        Unable_To_Consume_Stat_Change_Item = 10, // CField::IsUnableToConsumeStatChangeItem = CField *this 
+        Unable_To_Change_Party_Boss = 11, // CField::IsUnableToChangePartyBoss = CField *this 
+        Unknown1 = 12,
+        Unable_To_Use_Wedding_Invitation_Item = 13, // CField::IsUnableToUseWeddingInvitationItem = CField *this 
+        Unable_To_Use_Cash_Weather = 14, // CField::IsUnableToUseCashWeatherItem = CField *this 
+        Unable_To_Use_Pet = 15, // CField::IsUnableToUsePet = CField *this 
+        Unknown2 = 13,
+        Unknown3 = 14,
+        Unknown4 = 15,
+        Unknown5 = 16,
+        Unable_To_Fall_Down = 17, // CField::IsUnableToFallDown = CField *this 
+        Unable_To_Summon_NPC = 18, // CField::IsUnaUnableToUseAntiMacroItembleToSummonNPC = CField *this 
+        Unknown6 = 19,
+        No_Damage_On_Falling = 20, // CField::IsNoDamageOnFalling = CField *this 
+        Unknown7 = 21,
+        Drop_Limit = 22, // CField::IsDropLimit = CField *this 
+        Unable_To_Use_Rocket_Boost = 23, // CField::IsUnableToUseRocketBoost = CField *this
+        Unknown8 = 24,
+        No_Quest_Alert = 25, // CField::IsNoQuestAlert = CField *this 
+        Unknown9 = 26,
+        Auto_Expand_Minimap = 27, // CField::IsAutoExpandMinimap = CField *this 
+        Move_Skill_Only = 28, // CField::IsMoveSkillOnly = CField *this 
+    }
+
+    public static class FieldLimitTypeExtension
+    {
+        public static bool Check(int type, int fieldLimit)
+        {
+            return ((fieldLimit >> type) & 1) != 0;
+        }
+
+        public static bool Check(this WzFieldLimitType type, int fieldLimit)
+        {
+            return ((fieldLimit >> (int)type) & 1) != 0;
+        }
+
+        public static int GetMaxFieldLimitType()
+        {
+            int max = 0;
+            foreach (WzFieldLimitType limitType in Enum.GetValues(typeof(WzFieldLimitType)))
+            {
+                if ((int)limitType > max)
+                    max = (int)limitType;
+            }
+            return max;
+        }
+    }
+}
