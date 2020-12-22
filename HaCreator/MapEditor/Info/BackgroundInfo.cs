f78bbb2e318a7187d53a041ebc97e6d73f899b7d
LastBattle <4586194+lastbattle@users.noreply.github.com>
2020-12-22 17:16:35 +0800

    [HaCreator] Fix issues loading maps with missing 'bg'

--- HaCreator/MapEditor/Info/BackgroundInfo.cs
+++ HaCreator/MapEditor/Info/BackgroundInfo.cs
@@ -7,6 +7,7 @@
 using HaCreator.MapEditor.Instance;
 using HaCreator.Properties;
 using HaCreator.Wz;
+using MapleLib.Helpers;
 using MapleLib.WzLib;
 using MapleLib.WzLib.Spine;
 using MapleLib.WzLib.WzProperties;
@@ -64,6 +65,13 @@ namespace HaCreator.MapEditor.Info
 
             WzImage bsImg = Program.InfoManager.BackgroundSets[bS];
             WzImageProperty bgInfoProp = bsImg[type == BackgroundInfoType.Animation ? "ani" : type == BackgroundInfoType.Spine ? "spine" : "back"]?[no];
+
+            if (bgInfoProp == null)
+            {
+                string logError = string.Format("Background image {0}/{1} is null, {2}", bS, no, bsImg.ToString());
+                MapleLib.Helpers.ErrorLogger.Log(ErrorLevel.IncorrectStructure, logError);
+                return null;
+            }
 
             if (type == BackgroundInfoType.Spine)
             {
