e56a2f10f79a46fa6fc59618ea1561c472ece9ee
lastbattle <4586194+lastbattle@users.noreply.github.com>
2023-06-13 17:34:34 +0800

    [UnitTest] UnitTest_MapleLib.cs for CCrc32::GetCrc32

---
+++ UnitTest_WzFile/UnitTest_MapleLib.cs
@@ -0,0 +1,33 @@
+﻿using MapleLib;
+using MapleLib.ClientLib;
+using MapleLib.WzLib;
+using MapleLib.WzLib.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace UnitTest_WzFile {
+    [TestClass]
+    public class UnitTest_MapleLib {
+
+
+        public UnitTest_MapleLib() {
+        }
+
+        /// <summary>
+        /// Test CCrc32::GetCrc32 calculation
+        /// </summary>
+        [TestMethod]
+        public void TestCrcCalculation() {
+            int useVersion = 200;
+
+            uint crc_firstRun = CCrc32.GetCrc32(useVersion, 0, false, false);
+            Assert.IsTrue(crc_firstRun == 2384409922, "Expected value = (2,384,409,922), got {0}", crc_firstRun.ToString());
+
+            uint crc = CWvsPhysicalSpace2D.GetConstantCRC(useVersion);
+            Assert.IsTrue(crc == 1696968404, "Expected value = (crc = 1,696,968,404), got {0}", crc.ToString());
+        }
+    }
+}
