c5af9565a2fb5f47e7e9fc95770ac870758dde46
lastbattle <4586194+lastbattle@users.noreply.github.com>
2024-06-25 15:05:19 +0800

    [HaCreator] fix a map file parsing issue with 64-bit wz

--- HaSharedLibrary/Wz/WzInfoTools.cs
+++ HaSharedLibrary/Wz/WzInfoTools.cs
@@ -4,17 +4,12 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-using HaSharedLibrary;
 using MapleLib;
 using MapleLib.WzLib;
 using MapleLib.WzLib.WzProperties;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using XNA = Microsoft.Xna.Framework;
 
 namespace HaSharedLibrary.Wz
 {
@@ -260,9 +255,10 @@ namespace HaSharedLibrary.Wz
             if (mapParent == null) 
                 return null;
 
-            if (fileManager.Is64Bit) {
+            if (fileManager.Is64Bit) { // is WzFile
                 return (WzImage)mapParent;
             }
+            // is WzDirectory
             string mapIdNamePadded = AddLeadingZeros(mapid, 9) + ".img";
             return (WzImage)mapParent?[mapIdNamePadded];
         }
@@ -270,13 +266,13 @@ namespace HaSharedLibrary.Wz
         /// <summary>
         /// Finds a map directory from the list of Map.wzs
         /// On pre-bb client (BETA)
-        /// Data.wz/Map/Map/Map1/10000000.img
+        /// Data.wz/Map/Map/Map1/10000000.img (WzDirectory)
         /// 
         /// On pre 64-bit client:
-        /// Map.wz/Map/Map1/10000000.img
+        /// Map.wz/Map/Map1/10000000.img (WzDirectory)
         /// 
         /// On post 64-bit client:
-        /// Map/Map/Map1/Map1_000.wz/10000000.img
+        /// Map/Map/Map1/Map1_000.wz/10000000.img (WzFile)
         /// </summary>
         /// <param name="mapid"></param>
         /// <returns></returns>
@@ -285,20 +281,21 @@ namespace HaSharedLibrary.Wz
             string mapcat = fileManager.Is64Bit ? mapIdNamePadded.Substring(0, 1) : "Map" + mapIdNamePadded.Substring(0, 1);
             string baseDir = fileManager.Is64Bit ? "map\\map\\map" + mapcat : "map";
 
-            WzObject mapObject = fileManager.FindWzImageByName(baseDir, fileManager.Is64Bit ? mapIdNamePadded : "Map");
+            WzObject mapObjectWzDir = fileManager.FindWzImageByName(baseDir, fileManager.Is64Bit ? string.Empty : "Map");
 
             if (fileManager.Is64Bit) {
-                return (WzDirectory)mapObject;
+                Debug.WriteLine("Init map: {0}\\{1}", baseDir, mapIdNamePadded);
+                return (WzDirectory)mapObjectWzDir?[mapcat];
             }
             else {
-                WzDirectory mapImage = (WzDirectory) mapObject?[mapcat];
+                WzDirectory mapImage = (WzDirectory)mapObjectWzDir?[mapcat];
                 return mapImage;
             }
         }
 
-        public static Color XNAToDrawingColor(XNA.Color c)
+        public static Color XNAToDrawingColor(Microsoft.Xna.Framework.Color c)
         {
-            return Color.FromArgb(c.A, c.R, c.G, c.B);
+            return System.Drawing.Color.FromArgb(c.A, c.R, c.G, c.B);
         }
     }
 }
