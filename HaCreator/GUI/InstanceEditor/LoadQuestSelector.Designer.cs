06136a7395db31f02af4c88a9d0379572e822b92
lastbattle <4586194+lastbattle@users.noreply.github.com>
2024-09-02 12:33:53 +0800

    -

---
+++ HaCreator/GUI/InstanceEditor/LoadQuestSelector.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace HaCreator.GUI.InstanceEditor
+{
+    partial class LoadQuestSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox_npcList = new System.Windows.Forms.ListBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.button_select = new System.Windows.Forms.Button();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.label_questName = new System.Windows.Forms.Label();
+            this.label_questSummary = new System.Windows.Forms.Label();
+            this.searchBox = new HaCreator.CustomControls.WatermarkTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 20);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox_npcList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(400, 641);
+            this.splitContainer1.SplitterDistance = 555;
+            this.splitContainer1.SplitterWidth = 1;
+            this.splitContainer1.TabIndex = 9;
+            // 
+            // listBox_npcList
+            // 
+            this.listBox_npcList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_npcList.FormattingEnabled = true;
+            this.listBox_npcList.Location = new System.Drawing.Point(0, 0);
+            this.listBox_npcList.Name = "listBox_npcList";
+            this.listBox_npcList.Size = new System.Drawing.Size(400, 555);
+            this.listBox_npcList.TabIndex = 0;
+            this.listBox_npcList.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox_itemList_drawItem);
+            this.listBox_npcList.MeasureItem += new System.Windows.Forms.MeasureItemEventHandler(this.listBox_itemList_measureItem);
+            this.listBox_npcList.SelectedIndexChanged += new System.EventHandler(this.listBox_itemList_SelectedIndexChanged);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer3);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.button_select);
+            this.splitContainer2.Size = new System.Drawing.Size(400, 85);
+            this.splitContainer2.SplitterDistance = 280;
+            this.splitContainer2.SplitterWidth = 1;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // button_select
+            // 
+            this.button_select.Enabled = false;
+            this.button_select.Location = new System.Drawing.Point(3, 0);
+            this.button_select.Name = "button_select";
+            this.button_select.Size = new System.Drawing.Size(113, 82);
+            this.button_select.TabIndex = 0;
+            this.button_select.Text = "Select";
+            this.button_select.UseVisualStyleBackColor = true;
+            this.button_select.Click += new System.EventHandler(this.button_select_Click);
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.label_questName);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.label_questSummary);
+            this.splitContainer3.Size = new System.Drawing.Size(280, 85);
+            this.splitContainer3.SplitterDistance = 25;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // label_questName
+            // 
+            this.label_questName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_questName.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_questName.Location = new System.Drawing.Point(0, 0);
+            this.label_questName.Name = "label_questName";
+            this.label_questName.Size = new System.Drawing.Size(280, 25);
+            this.label_questName.TabIndex = 0;
+            // 
+            // label_questSummary
+            // 
+            this.label_questSummary.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label_questSummary.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_questSummary.Location = new System.Drawing.Point(0, 0);
+            this.label_questSummary.Name = "label_questSummary";
+            this.label_questSummary.Size = new System.Drawing.Size(280, 56);
+            this.label_questSummary.TabIndex = 0;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.searchBox.ForeColor = System.Drawing.Color.Gray;
+            this.searchBox.Location = new System.Drawing.Point(0, 0);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(400, 20);
+            this.searchBox.TabIndex = 8;
+            this.searchBox.Text = "Type here to search";
+            this.searchBox.WatermarkActive = true;
+            this.searchBox.WatermarkText = "Type here";
+            this.searchBox.TextChanged += new System.EventHandler(this.searchBox_TextChanged);
+            // 
+            // LoadQuestSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 661);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.searchBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LoadQuestSelector";
+            this.Text = "Select a quest";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Load_KeyDown);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CustomControls.WatermarkTextBox searchBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox_npcList;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button button_select;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Label label_questName;
+        private System.Windows.Forms.Label label_questSummary;
+    }
+}
---
+++ HaCreator/GUI/InstanceEditor/LoadQuestSelector.cs
@@ -0,0 +1,291 @@
+/*Copyright(c) 2024, LastBattle https://github.com/lastbattle/Harepacker-resurrected
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using MapleLib.WzLib.WzStructure;
+using MapleLib.WzLib;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HaCreator.MapEditor.Instance.Shapes;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Threading;
+using MapleLib.WzLib.WzProperties;
+using MapleLib.WzLib.WzStructure.Data.ItemStructure;
+
+namespace HaCreator.GUI.InstanceEditor
+{
+    public partial class LoadQuestSelector : Form
+    {
+        private bool _isLoading = false;
+        private bool _bItemsLoaded = false;
+
+        // dictionary
+        private readonly List<string> questNames = new List<string>(); // cache
+
+
+        private string _selectedQuestId = string.Empty;
+        /// <summary>
+        /// The selected itemId in the listbox
+        /// </summary>
+        public string SelectedQuestId
+        {
+            get { return _selectedQuestId; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LoadQuestSelector()
+        {
+            InitializeComponent();
+
+            // load items
+            load();
+        }
+
+        #region Window
+
+        /// <summary>
+        /// Loads the item on start of the window
+        /// </summary>
+        private void load()
+        {
+            _isLoading = true;
+            try
+            {
+                // Maps
+                foreach (KeyValuePair<string, WzSubProperty> kvp in Program.InfoManager.QuestInfos)
+                {
+                    string key = kvp.Key;
+                    WzSubProperty questProp = kvp.Value;
+
+                    // Quest name
+                    string questName = (questProp["name"] as WzStringProperty)?.Value ?? "NO NAME";
+
+                    string combinedId_ItemName = string.Format("[{0}] - {1}", key, questName);
+                    questNames.Add(combinedId_ItemName);
+                }
+                questNames.Sort();
+
+                object[] itemObjs = questNames.Cast<object>().ToArray();
+                listBox_npcList.Items.AddRange(itemObjs);
+            }
+            finally
+            {
+                _isLoading = false;
+                _bItemsLoaded = true;
+            }
+        }
+
+        private void Load_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Close(); // close window
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                //loadButton_Click(null, null);
+            }
+        }
+        #endregion
+
+        #region Search box
+        private string _previousSeachText = string.Empty;
+        private CancellationTokenSource _existingSearchTaskToken = null;
+
+        /// <summary>
+        /// Searchbox text changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void searchBox_TextChanged(object sender, EventArgs e)
+        {
+            TextBox searchBox = (TextBox)sender;
+            string searchText = searchBox.Text.ToLower();
+
+            if (_previousSeachText == searchText)
+                return;
+            _previousSeachText = searchText; // set
+
+            // start searching
+            searchItemInternal(searchText);
+        }
+
+        /// <summary>
+        /// Search and filters map according to the user's query
+        /// </summary>
+        /// <param name="searchText"></param>
+        public void searchItemInternal(string searchText)
+        {
+            if (!_bItemsLoaded)
+                return;
+
+            // Cancel existing task if any
+            if (_existingSearchTaskToken != null && !_existingSearchTaskToken.IsCancellationRequested)
+            {
+                _existingSearchTaskToken.Cancel();
+            }
+
+            // Clear 
+            listBox_npcList.Items.Clear();
+            if (searchText == string.Empty)
+            {
+                var filteredItems = questNames.Where(kvp =>
+                {
+                    return true;
+                }).Select(kvp => kvp) // or kvp.Value or any transformation you need
+                  .Cast<object>()
+                  .ToArray();
+
+                listBox_npcList.Items.AddRange(filteredItems);
+
+                listBox_itemList_SelectedIndexChanged(null, null);
+            }
+            else
+            {
+
+                Dispatcher currentDispatcher = Dispatcher.CurrentDispatcher;
+
+                // new task
+                _existingSearchTaskToken = new CancellationTokenSource();
+                var cancellationToken = _existingSearchTaskToken.Token;
+
+                Task t = Task.Run(() =>
+                {
+                    Thread.Sleep(500); // average key typing speed
+
+                    List<string> itemsFiltered = new List<string>();
+                    foreach (string map in questNames)
+                    {
+                        if (_existingSearchTaskToken.IsCancellationRequested)
+                            return; // stop immediately
+
+                        // Filter by string first
+                        if (map.ToLower().Contains(searchText))
+                        {
+                            itemsFiltered.Add(map);
+                        }
+                    }
+
+                    currentDispatcher.BeginInvoke(new Action(() =>
+                    {
+                        foreach (string map in itemsFiltered)
+                        {
+                            if (_existingSearchTaskToken.IsCancellationRequested)
+                                return; // stop immediately
+
+                            listBox_npcList.Items.Add(map);
+                        }
+
+                        if (listBox_npcList.Items.Count > 0)
+                        {
+                            listBox_npcList.SelectedIndex = 0; // set default selection to reduce clicks
+                        }
+                    }));
+                }, cancellationToken);
+
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// On list box selection changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBox_itemList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (_isLoading || listBox_npcList.SelectedItem == null)
+                return;
+
+            string selectedItem = listBox_npcList.SelectedItem as string;
+
+            const string pattern = @"\[(\d+)\]"; //  "[123] - SampleItem"
+            Match match = Regex.Match(selectedItem, pattern);
+
+            if (match.Success)
+            {
+                string questId = match.Groups[1].Value;
+
+                string questName = "NO NAME";
+                int index = selectedItem.IndexOf("] - ") + 3;
+                if (index > 0)
+                {
+                    questName = selectedItem.Substring(index);
+                }
+
+                if (Program.InfoManager.QuestInfos.ContainsKey(questId)) {
+                    WzSubProperty questProp = Program.InfoManager.QuestInfos[questId];
+
+                    string summary = (questProp["summary"] as WzStringProperty)?.Value;
+
+                    label_questSummary.Text = summary;
+                } else
+                {
+                    label_questSummary.Text = string.Empty;
+                }
+
+                // label desc
+                label_questName.Text = questName;
+
+                // set selected itemid
+                this._selectedQuestId = questId;
+                this.button_select.Enabled = true;
+                return;
+            }
+            label_questName.Text = string.Empty;
+            this._selectedQuestId = string.Empty;
+            button_select.Enabled = false;
+        }
+
+        private void listBox_itemList_measureItem(object sender, MeasureItemEventArgs e)
+        {
+            //e.ItemHeight = (int)e.Graphics.MeasureString(listBox_itemList.Items[e.Index].ToString(), listBox_itemList.Font, listBox_itemList.Width).Height;
+        }
+
+        private void listBox_itemList_drawItem(object sender, DrawItemEventArgs e)
+        {
+            //e.DrawBackground();
+            //e.DrawFocusRectangle();
+
+            //e.Graphics.DrawString(listBox_itemList.Items[e.Index].ToString(), e.Font, new SolidBrush(e.ForeColor), e.Bounds);
+        }
+
+        /// <summary>
+        /// On select button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_select_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
